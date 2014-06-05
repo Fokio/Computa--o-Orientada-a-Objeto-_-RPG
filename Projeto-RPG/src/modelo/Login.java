3a3f24db154cbec67e1951718d37fb9556641ef0
DeivisHenri <deivis_shaft@hotmail.com>
2014-06-05 15:00:45 -0300

    Add!
    Coloquei mais a classe de ManipulacaoArquivo para minupular os arquivos,
    Teste como sendo a classe que tem o main!

--- Projeto-RPG/src/modelo/Login.java
+++ Projeto-RPG/src/modelo/Login.java
@@ -2,20 +2,26 @@
  * A classe login irá garantir que o usúario faço o login no sistema do jogo. 
  */
 
+// Pacote Modelo
 package modelo;
 
 public class Login
 {
+    // Variável que contem o nome para logar no sistema.
     private String nome;
+    // Variável que tem a senha do usúario para logar no sistema.
     private String senha;
     
+    // Construtor padrão.
     public Login(){}
+    // Construtor completo.
     public Login(String nome, String senha)
     {
          this.setNome(nome);
          this.setSenha(senha);
     }
     
+    // Método para setar o nome.
     public void setNome(String nome)
     {
          try{
@@ -30,11 +36,13 @@ public class Login
             throw new NullPointerException();
         }
     }
+    // Método para recuperar o conteúdo que está na variável nome.
     public String getNome()
     {
          return this.nome;
     }
     
+    // Método para atribuir valor a variável senha.
     public void setSenha(String senha)
     {
          try{
@@ -49,7 +57,8 @@ public class Login
               throw new NullPointerException();
          } 
     }
-    public String getSEnha()
+    //Método para recuperar o conteúdo que está na variável senha.
+    public String getSenha()
     {
          return this.senha;
     }
---
+++ Projeto-RPG/src/view/Teste.java
@@ -0,0 +1,33 @@
+/*
+##     ## ######## ##     ## 
+##     ## ##       ##     ## 
+##     ## ##       ##     ## 
+##     ## ######   ##     ## 
+##     ## ##        ##   ##  
+##     ## ##         ## ##   
+ #######  ##          ###    
+
+Universidade Federal de Viçosa - Campus Rio Paranaíba
+SIN141 - Computação Orientada a Objetos
+
+Projeto: Jogo RPG
+
+Desenvolvido por:
+Deivis Henrique Alves - Matrícula 2480
+Danilo Lemos Prado - Matrícula 2489
+*/
+
+// Pacote View
+package view;
+
+// Importação das bibliotecas necessarias.
+import modelo.*;
+import persistencia.*;
+
+public class Teste
+{
+    public static void main(String arg[])
+    {
+         
+    }
+}
