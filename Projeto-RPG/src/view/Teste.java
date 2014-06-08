386ddc3367e0f5480dacb08e0593beff5fc703c5
Fokio <danilollemes@hotmail.com>
2014-06-07 23:42:03 -0300

    Update Teste.java

--- Projeto-RPG/src/view/Teste.java
+++ Projeto-RPG/src/view/Teste.java
@@ -9,12 +9,14 @@
 
 Universidade Federal de Viçosa - Campus Rio Paranaíba
 SIN141 - Computação Orientada a Objetos
-
+Professor: 
 Projeto: Jogo RPG
 
 Desenvolvido por:
 Deivis Henrique Alves - Matrícula 2480
-Danilo Lemos Prado - Matrícula 2489
+Danilo Prado Lemes - Matrícula 2489
+Gustavo Amaral Silva - 2464
+
 */
 
 // Pacote View
