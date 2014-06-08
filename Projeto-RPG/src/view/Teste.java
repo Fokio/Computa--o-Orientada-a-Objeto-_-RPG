3f4f7aa2ae06c9e75831ce248b40c927c5d1bc24
guaamarals <gustavo-amarals@hotmail.com>
2014-06-08 19:18:55 -0300

    Revert "Update Teste.java"
    This reverts commit 386ddc3367e0f5480dacb08e0593beff5fc703c5.

--- Projeto-RPG/src/view/Teste.java
+++ Projeto-RPG/src/view/Teste.java
@@ -9,14 +9,12 @@
 
 Universidade Federal de Viçosa - Campus Rio Paranaíba
 SIN141 - Computação Orientada a Objetos
-Professor: 
+
 Projeto: Jogo RPG
 
 Desenvolvido por:
 Deivis Henrique Alves - Matrícula 2480
-Danilo Prado Lemes - Matrícula 2489
-Gustavo Amaral Silva - 2464
-
+Danilo Lemos Prado - Matrícula 2489
 */
 
 // Pacote View
